c396d8d658f6556cc67231a298ca157436b6c909
linzowo <linzowo@outlook.com>
2020-01-17 17:32:51 +0800

    组件开发-gotop-002：完成回到顶部组件开发

--- src/library/gotop/gotop.js
+++ src/library/gotop/gotop.js
@@ -1,4 +1,4 @@
-//
+// 侧边回到顶部组件
 
 /**
  * 返回一个a标签字符串
@@ -11,40 +11,28 @@ function getLink(content, href) {
   return `<a href='${href}'>${content}</a>`;
 }
 
-// 1 将css文件和js放入文件夹中
-// 2 在页面中创建一个容器
-// 3 引入js文件
-// 4 实例化对象 gotop 并传入指定参数
-// 5 自动引入css文件 并向页面中插入结构
-
 /**
- * 参数：
- *
- * css文件地址 ==》 必须
- * 根节点 ==》 必须 可以是dom元素也可以是选择器
- * 数量
- * 文本内容 | 背景图片位置 ===》 数组
- *
- * css
- * 位置
- * 边距
- * margin
- * 大小
- *
+ * @param {DOM | querySelector} root 组件根节点元素或者元素选择器
+ * @param {String} cssPath 样式文件相对路径
+ * @param {Object} obj 设置参数包含下列对象
+ * {
+ *    href:{"linkURL":textContent}
+ * }
  */
-
 class GoTop {
   constructor(root, cssPath, obj) {
     // 根节点
     this.root = root.nodeType === 1 ? root : document.querySelector(root);
+    // 为根节点添加样式名称
+    this.root.classList.add("gotop-container");
     // css文件路径
     this.cssPath = cssPath;
 
     // 其他参数
-    // 内容参数或者图片地址 ==》 数组或字符串
-    // 导航链接的地址 ==》 数组或者字符串
     this.hrefObj = obj.href;
 
+    // 存储a标签元素的数组
+    this.LinkEleList = null;
 
     // 初始化对象
     this.init();
@@ -70,11 +58,13 @@ class GoTop {
 
   setHtml() {
     let res = "";
-    console.log(this.hrefObj);
-    
+
     for (const key in this.hrefObj) {
-      res += getLink(key, this.hrefObj[key]);
+      res += getLink(this.hrefObj[key], key);
     }
     this.root.innerHTML = res;
+
+    // 返回生成的a标签让用户可以单独操作
+    this.LinkEleList = this.root.querySelectorAll("a");
   }
 }
