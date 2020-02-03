4a2bfc379bfda9ca3699fb2b9c7b20a502240c14
linzowo <linzowo@outlook.com>
2020-02-03 21:53:31 +0800

    feat(index-mod): 完成轮播图的js动画及相关控制关联效果
    添加了common模块用于封装各种公共方法，同时封装了关于获取兄弟元素的兼容性方法，获取元素样式值的方法，实现缓动动画的方法。通过组合这些方法实现了，主页模块中类似轮播图的区域的关联效果。

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 // 入口文件
 
+// 引入公共方法
+import {
+  animate,
+  getStyle,
+  getType,
+  getFirstElementChild,
+  getLastElementChild,
+  getPreviousElement,
+  getNextElement,
+  getBrotherElementAll
+} from "./js/common.js";
 // 引入侧边固定按钮组件
 import GoTop from "./js/util/gotop.js";
 
@@ -18,7 +29,7 @@ import "./css/fixed-flag.css";
 import "./pages/common/common-index-nav/common-index-nav.css";
 
 // 创建一个侧边固定按钮，并设置相关属性及样式
-let gotop = new GoTop("#side-btn",null, {
+let gotop = new GoTop("#side-btn", null, {
   href: {
     "#1": null,
     "#2": "反馈",
@@ -32,3 +43,100 @@ btn_gotop.classList.add("side-gotop");
 // 为网页播放器添加样式
 btn_player.classList.add("side-player");
 
+// 轮播左右按钮
+let jump_btn_arr = document.querySelectorAll(".js-jump");
+
+// 为跳转按钮注册点击事件
+jump_btn_arr.forEach(ele => {
+  ele.addEventListener("click", function() {
+    // 判断当前点击的按钮是哪个区域的按钮
+    // 获取要操作的目标区域
+    let stat_arr = this.dataset.stat.split(".");
+    let targetEle = document.querySelector("." + stat_arr[2] + "-list");
+    // 父元素宽度--一次动画的移动距离
+    let parentWidth = targetEle.parentElement.offsetWidth;
+
+    // 当前轮播标记所在的元素
+    let slide_switch_item_current = document.querySelector(
+      ".slide-switch-item-current"
+    );
+    // 移除当前标记
+    slide_switch_item_current.classList.remove("slide-switch-item-current");
+
+    // 判断当前数据是文本还是数字
+    if (isNaN(parseInt(this.dataset.p))) {
+      // 不是数字
+      let next_slide_switch_item = getNextElement(slide_switch_item_current);
+      let prev_slide_switch_item = getPreviousElement(
+        slide_switch_item_current
+      );
+      let first_slide_switch_item = getFirstElementChild(
+        slide_switch_item_current.parentElement
+      );
+      let last_slide_switch_item = getLastElementChild(
+        slide_switch_item_current.parentElement
+      );
+      // let all_brother_slide_switch_item = getBrotherElementAll(
+      //   slide_switch_item_current
+      // );
+
+      switch (this.dataset.p) {
+        case "prev":
+          // 切换轮播标记
+          prev_slide_switch_item
+            ? prev_slide_switch_item.classList.add("slide-switch-item-current")
+            : last_slide_switch_item.classList.add("slide-switch-item-current");
+
+          // 执行动画
+          if (!prev_slide_switch_item) {
+            targetEle.style.left = "0px";
+          }
+          animate(targetEle, {
+            left: parseInt(getStyle(targetEle, "left")) - parentWidth
+          });
+          break;
+        case "next":
+          next_slide_switch_item
+            ? next_slide_switch_item.classList.add("slide-switch-item-current")
+            : first_slide_switch_item.classList.add(
+                "slide-switch-item-current"
+              );
+
+          if (next_slide_switch_item == last_slide_switch_item) {
+            targetEle.style.left =
+              -1 *
+                slide_switch_item_current.parentElement.childElementCount *
+                parentWidth +
+              "px";
+            // targetEle.style.left = "0px";
+          }
+          animate(targetEle, {
+            left: parseInt(getStyle(targetEle, "left")) + parentWidth
+          });
+          break;
+
+        default:
+          break;
+      }
+    } else {
+      // 是数字
+
+      this.classList.add("slide-switch-item-current");
+      animate(targetEle, {
+        left: parseInt(this.dataset.p) * -parentWidth
+      });
+    }
+
+    // 判断当前按钮的类别
+
+    // 执行指定操作
+  });
+});
+
+// 轮播图动画效果
+
+// 切换轮播标记
+function toggleTag(currentEle, targetEle, className) {
+  currentEle.classList.remove(className);
+  targetEle.classList.add(className);
+}
---
+++ src/js/common.js
@@ -0,0 +1,183 @@
+// 公共方法
+
+/**
+ * 获取元素的css样式值
+ * @param {DOM} element 要获取样式的元素
+ * @param {String} attr 要获取的样式值
+ * @return {String} 返回值始终是一个数字加单位不会出现百分比
+ */
+let getStyle = function(element, attr) {
+  return window.getComputedStyle
+    ? window.getComputedStyle(element, null)[attr]
+    : element.currentStyle[attr];
+};
+
+/**
+ * 判断用户传入的值是百分比还是px
+ * @param {String} cssValue 样式值
+ * @return {Object} res["value"] 当前css样式的数字值
+ * res["type"] 当前css样式的单位 % || px ...
+ */
+let getType = function(cssValue) {
+  let res = {};
+  res["value"] = cssValue.match(/\d+\.?\d*/)[0];
+  res["type"] = cssValue.match(/\D+$/)[0];
+  return res;
+};
+
+// js动画效果
+/**
+ * js缓动动画
+ * @param {DOM} element 动画效果元素
+ * @param {Object} json css属性和参数 key为String value为Number
+ * @param {function} callback 回调函数。动画执行完毕后调用
+ */
+let animate = function(element, json, callback) {
+  // 如果当前元素正在执行动画效果那么就不在继续添加动画效果
+  if (element.time_id) return;
+
+  // 为元素添加动画效果
+  element.time_id = setInterval(() => {
+    //   判断是否所有动画都完成的标志
+    let flag = true;
+    let target, current, step;
+    for (let attr in json) {
+      if (attr === "opacity") {
+        // opacity的取值范围为 0-1
+        target = parseFloat(json[attr]) * 100;
+        current = parseFloat(getStyle(element, attr)) * 100;
+        step = (target - current) / 10;
+        step = step < 0 ? Math.floor(step) : Math.ceil(step);
+        current = Math.abs(step) < 0.3 ? target : current + step;
+        element.style[attr] = current / 100;
+      } else if (attr === "zIndex") {
+        element.style[attr] = parseInt(json[attr]) ? parseInt(json[attr]) : 0;
+      } else {
+        // 目标值 将其转换为数字，确保用户传入的内容能够被识别
+        target = parseFloat(json[attr]);
+        // 获取当前位置信息
+        current = parseFloat(getStyle(element, attr));
+        // 计算步数 同时确定移动的方向（正负）
+        step = (target - current) / 10;
+        // 因为浮点数存在计算精度的问题，所以先取整再计算，根据正负取整
+        step = step < 0 ? Math.floor(step) : Math.ceil(step);
+        // 获取移动后的位置
+        current = Math.abs(step) < 0.1 ? target : current + step;
+        // 将值应用到元素上
+        element.style[attr] = current + "px";
+      }
+
+      if (target != current) {
+        flag = false;
+      }
+    }
+
+    // 结束动画
+    if (flag) {
+      clearInterval(element.time_id);
+      element.time_id = null;
+      if (callback) {
+        callback();
+      }
+    }
+  }, 20);
+};
+
+/**
+ * 获取任意一个父级元素的第一个子元素
+ * @param {DOM} element
+ */
+let getFirstElementChild = function(element) {
+  if (element.firstElementChild) {
+    return element.firstElementChild;
+  } else {
+    // 如果其他浏览器使用firstChild获取到的不是元素，仍然是节点的情况下，我们就需要判断其获取到的是标签还是其他类型的节点
+    var node = element.firstChild; // 将获取到的第一个内容存储起来
+    while (node && node.nodeType != 1) {
+      // 如果获取到的内容不是标签就检查下一个内容
+      node = node.nextSibling;
+    } // end while
+    return node;
+  }
+};
+
+/**
+ * 获取任意一个父级元素的最后一个子元素
+ * @param {DOM} element
+ */
+let getLastElementChild = function(element) {
+  if (element.lastElementChild) {
+    return element.lastElementChild;
+  } else {
+    var node = element.lastChild;
+    while (node && node.nodeType != 1) {
+      node = node.previousSibling;
+    } // end while
+    return node;
+  }
+};
+
+/**
+ * 获取任意元素上一个兄弟元素
+ * @param {DOM} element 要获取兄弟元素的元素
+ */
+let getPreviousElement = function(element) {
+  if (element.previousElementSibling) {
+    return element.previousElementSibling;
+  } else {
+    var node = element.previousSibling;
+    while (node && node.nodeType != 1) {
+      node = node.previousSibling;
+    } // end while
+    return node;
+  }
+};
+
+/**
+ * 获取任意元素下一个兄弟元素
+ * @param {DOM} element 要获取兄弟元素的元素
+ */
+let getNextElement = function(element) {
+  if (element.nextElementSibling) {
+    return element.nextElementSibling;
+  } else {
+    var node = element.nextSibling;
+    while (node && node.nodeType != 1) {
+      node = node.nextSibling;
+    } // end while
+    return node;
+  }
+};
+
+/**
+ * 获取当前元素的所有兄弟元素
+ * @param {DOM} element 
+ */
+let getBrotherElementAll = function (element) {
+  var broElementList = [];
+  var preEle = getPreviousElement(element);
+  var nextEle = getNextElement(element);
+  while (preEle || nextEle) {
+    if (preEle) {
+      broElementList.push(preEle);
+      preEle = getPreviousElement(preEle);
+    } else if (nextEle) {
+      broElementList.push(nextEle);
+      nextEle = getNextElement(nextEle);
+    } // end if
+  } // end while
+  return broElementList;
+}
+
+
+// 导出必要函数
+export {
+  animate,
+  getStyle,
+  getType,
+  getFirstElementChild,
+  getLastElementChild,
+  getPreviousElement,
+  getNextElement,
+  getBrotherElementAll
+};
